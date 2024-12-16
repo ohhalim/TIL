8d0ccbd1c6f91e6f05d70b0c50c8f8ca7d55844c
ohhalim <ohhalim777@gmail.com>
2024-12-16 19:01:20 +0900

    241216

--- django/my_first_pjt/articles/models.py
+++ django/my_first_pjt/articles/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Article(models.Model):
     title =  models.CharField(max_length=50)
     content = models.TextField()
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
