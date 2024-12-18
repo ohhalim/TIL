2d86f75fdce9c470b11f80011f887b5c86c49693
ohhalim <ohhalim777@gmail.com>
2024-12-18 20:03:49 +0900

    241218

--- django/my_first_pjt/articles/urls.py
+++ django/my_first_pjt/articles/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [  
     path('', views.articles, name="articles"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/", views.article_detail, name="article_detail"),
+    
     path("index/", views.index, name="index"),
     path('data_throw/', views.data_throw, name ='data_throw'),
     path('data_catch/', views.data_catch, name ='data_catch'), 
 ]
 
-   
--- django/my_first_pjt/articles/views.py
+++ django/my_first_pjt/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import rediect, render
 from .models import Article
 
 # Create your views here.
@@ -7,12 +7,29 @@ def index(request):
         # 탬플릿을 랜더해서 리턴할수있다
 
 def articles(request):
-    articles = Article.objects.all()
-    return render(request, 'articles.html')
+    articles = Article.objects.all().order_by("-created_at")
+    context = {
+        "articles": articles,
+    }
+    return render(request, 'articles.html', context)
+
+def article_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {"article": article}
+    return render(request, "article_detail.html", context)
+
 
 def data_throw(request):
     return render(request, 'data_throw.html')
 
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    Article.objects.create(title=title, content=content)
+    return rediect("articles")
 
 def data_catch(request):
     data = request.GET.get("message")
